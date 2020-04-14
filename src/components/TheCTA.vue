1a27790aec0a955e8b7e3b227fb2126a5a9b6bd7
Philo van Kemenade <philovankemenade@gmail.com>
2020-04-14 21:33:17 +0200

    🔧 tweak CTA copy

--- src/components/TheCTA.vue
+++ src/components/TheCTA.vue
@@ -1,10 +1,11 @@
 <template>
-  <section>
-    Select a <span class="bb b--primary-accent">decade</span> in the bar chart
-    below. <br />
-    Then filter further by selecting a
-    <span class="bb b--secondary">Subject</span> or
-    <span class="bb b--secondary">Location</span> filter. <br />
-    Or try randomize<v-icon>shuffle</v-icon> the selection to be surprised.
-  </section>
+    <section>
+        Select a <span class="bb b--primary-accent">decade</span> in the bar
+        chart below. <br />
+        Then filter further by selecting a
+        <span class="bb b--secondary">Subject</span> or
+        <span class="bb b--secondary">Location</span> filter. <br />
+        Or try to randomize<v-icon>shuffle</v-icon> the selection and be
+        surprised.
+    </section>
 </template>
