f7f1b0695e3b483713b73613b7f8c8148c3ec7d8
Philo van Kemenade <philovankemenade@gmail.com>
2020-04-28 19:19:46 +0200

    change default theme to dark

--- .storybook/addon-vuetify/decorator.js
+++ .storybook/addon-vuetify/decorator.js
@@ -1,30 +1,35 @@
 // Imports
-import Vue from 'vue'
-import Vuetify from 'vuetify'
-import { makeDecorator } from '@storybook/addons'
+import Vue from "vue";
+import Vuetify from "vuetify";
+import { makeDecorator } from "@storybook/addons";
 
 // Utilities
-import deepmerge from 'deepmerge'
+import deepmerge from "deepmerge";
 
 // Vuetify
-import 'vuetify/dist/vuetify.min.css'
-import '@mdi/font/css/materialdesignicons.min.css'
+import "vuetify/dist/vuetify.min.css";
+import "@mdi/font/css/materialdesignicons.min.css";
 
-Vue.use(Vuetify)
+Vue.use(Vuetify);
 
 export default makeDecorator({
-  name: 'withVuetify',
-  parameterName: 'vuetify',
+  name: "withVuetify",
+  parameterName: "vuetify",
   wrapper: (storyFn, context, { parameters = {} }) => {
     // Reduce to one new URL?
-    const searchParams = new URL(window.location).searchParams
-    const dark = searchParams.get('eyes-variation') === 'dark'
-    const rtl = searchParams.get('eyes-variation') === 'rtl'
-    const vuetify = new Vuetify(deepmerge({
-      rtl,
-      theme: { dark },
-    }, parameters))
-    const WrappedComponent = storyFn(context)
+    const searchParams = new URL(window.location).searchParams;
+    const dark = searchParams.get("eyes-variation") !== "light";
+    const rtl = searchParams.get("eyes-variation") === "rtl";
+    const vuetify = new Vuetify(
+      deepmerge(
+        {
+          rtl,
+          theme: { dark },
+        },
+        parameters
+      )
+    );
+    const WrappedComponent = storyFn(context);
 
     return Vue.extend({
       vuetify,
@@ -36,6 +41,6 @@ export default makeDecorator({
           </v-container>
         </v-app>
       `,
-    })
+    });
   },
-})
+});
