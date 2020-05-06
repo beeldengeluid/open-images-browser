daa5f8f7c69cfbdda3b32c34f8ef2e840ffb5778
Philo van Kemenade <philovankemenade@gmail.com>
2020-05-06 13:08:45 +0200

    add knobs and version stories for StateStory

--- src/components/StateStory.stories.js
+++ src/components/StateStory.stories.js
@@ -1,12 +1,14 @@
 // Utilities
 import { storyFactory } from "../../.storybook/util/helpers";
 import { action } from "@storybook/addon-actions";
+import { withKnobs, object } from "@storybook/addon-knobs";
 
 // Components
 import { StateStory } from "./StateStory";
 
 export default {
   title: "StateStory",
+  decorators: [withKnobs],
   // Our exports that end in "Data" are not stories.
   excludeStories: /.*Data$/,
 };
@@ -28,9 +30,42 @@ export const actionsData = {
   onToggleActiveFilter: action("onToggleActiveFilter"),
 };
 
-export const stateData = {
+export const stateBothData = {
   activeFilters: {
     locations: ["Hilversum"],
+    subjects: ["wedstrijden"],
+  },
+};
+
+export const stateLocationsData = {
+  activeFilters: {
+    locations: ["Hilversum"],
+    subjects: [],
+  },
+};
+
+export const stateSubjectsData = {
+  activeFilters: {
+    locations: [],
+    subjects: ["wedstrijden"],
+  },
+};
+
+export const stateManyData = {
+  activeFilters: {
+    locations: ["Nederland", "Amsterdam", "Alkmaar", "Amstelveen"],
+    subjects: [
+      "wedstrijden",
+      "nationaal team Nederland",
+      "prijsuitreikingen",
+      "sport",
+    ],
+  },
+};
+
+export const stateEmptyData = {
+  activeFilters: {
+    locations: [],
     subjects: [],
   },
 };
@@ -41,22 +76,80 @@ export const computedData = {
   totalLength: 1000,
 };
 
-export const Default = () =>
-  story({
-    props: {
-      state: {
-        default: () => stateData,
-      },
-      computed: {
-        default: () => computedData,
-      },
-    },
-    methods: actionsData,
-    template: `
+const stateStoryTemplate = `
     <StateStory
       :state="state"
       :computed="computed"
       v-on:toggle-active-filter="onToggleActiveFilter"
     />
-  `,
+  `;
+
+export const NoFilters = () =>
+  story({
+    props: {
+      state: {
+        default: object("state", { ...stateEmptyData }),
+      },
+      computed: {
+        default: object("computed", { ...computedData }),
+      },
+    },
+    methods: actionsData,
+    template: stateStoryTemplate,
+  });
+
+export const BothFilters = () =>
+  story({
+    props: {
+      state: {
+        default: object("state", { ...stateBothData }),
+      },
+      computed: {
+        default: object("computed", { ...computedData }),
+      },
+    },
+    methods: actionsData,
+    template: stateStoryTemplate,
+  });
+
+export const LocationFilters = () =>
+  story({
+    props: {
+      state: {
+        default: object("state", { ...stateLocationsData }),
+      },
+      computed: {
+        default: object("computed", { ...computedData }),
+      },
+    },
+    methods: actionsData,
+    template: stateStoryTemplate,
+  });
+
+export const SubjectFilters = () =>
+  story({
+    props: {
+      state: {
+        default: object("state", { ...stateSubjectsData }),
+      },
+      computed: {
+        default: object("computed", { ...computedData }),
+      },
+    },
+    methods: actionsData,
+    template: stateStoryTemplate,
+  });
+
+export const ManyFilters = () =>
+  story({
+    props: {
+      state: {
+        default: object("state", { ...stateManyData }),
+      },
+      computed: {
+        default: object("computed", { ...computedData }),
+      },
+    },
+    methods: actionsData,
+    template: stateStoryTemplate,
   });
