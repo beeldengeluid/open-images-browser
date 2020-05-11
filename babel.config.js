ebb7320db59013a7d4d4ce7c1eaf23c8770f876e
Philo van Kemenade <philovankemenade@gmail.com>
2020-05-11 17:33:09 +0200

    🔧 fix error in storybook build due to babel preset ordering
    `.storybook/addon-show-vue-markup/register.js: Missing class properties transform.`

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset',
-    '@babel/preset-env',
-    '@babel/preset-react'
-  ]
-}
+    "@babel/preset-env",
+    "@babel/preset-react",
+    "@vue/cli-plugin-babel/preset",
+  ],
+};
