51da48436651ecc93f7c2562f596c1ce8351f33e
Philo van Kemenade <philovankemenade@gmail.com>
2020-09-23 22:17:21 +0200

    catch same page navigation error

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -618,6 +618,15 @@ export default {
             [filterType]: newValue,
           },
         }),
+      }).catch(err => {
+        // Ignore the vuex err regarding  navigating to the page they are already on.
+        if (
+          err.name !== 'NavigationDuplicated' &&
+          !err.message.includes('Avoided redundant navigation to current location')
+        ) {
+          // But print any other errors to the console
+          logError(err);
+        }
       });
     },
   },
