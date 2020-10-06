b62a6bc2f3b7afcaa97265d1ece7a86931feb404
Philo van Kemenade <philovankemenade@gmail.com>
2020-10-06 21:15:20 +0200

    tweak intro phrasing

--- src/components/TheIntro.vue
+++ src/components/TheIntro.vue
@@ -5,13 +5,13 @@
       <a
         href="https://www.vogin.nl/portfolio-item/mediascape-2019/"
         target="_blank"
-        >MediaScape↗︎</a
+        >MediaScape Project ↗︎</a
       >
-      investigation into
+      , investigating
       <a
         href="http://www.digitalhumanities.org/dhq/vol/9/1/000205/000205.html"
         target="_blank"
-        >generous interfaces↗︎</a
+        >generous interfaces ↗︎</a
       >
       for audio-visual collections.
     </p>
