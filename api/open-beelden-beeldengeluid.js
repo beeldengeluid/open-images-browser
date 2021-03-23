42729d3ccf7d4cb675dfd2d67f7189c06be7c897
Philo van Kemenade <philovankemenade@gmail.com>
2021-03-23 15:47:09 +0100

    refactor: use public ODL search API
    with respective client id and application token

--- api/open-beelden-beeldengeluid.js
+++ api/open-beelden-beeldengeluid.js
@@ -1,11 +1,11 @@
 const axios = require("axios");
-const CID = process.env.NISV_SEARCH_API_CID;
-const AT = process.env.NISV_SEARCH_API_AT;
+const CID = process.env.NISV_SEARCH_ODL_API_CID;
+const AT = process.env.NISV_SEARCH_ODL_API_AT;
+const URL = "https://search-odl.rdlabs.beeldengeluid.nl/api/v1.1/search/open-beelden-beeldengeluid";
 
 module.exports = (req, res) => {
   let axiosConfig = {
-    url:
-      "https://search.rdlabs.beeldengeluid.nl/api/v1.1/search/open-beelden-beeldengeluid",
+    url: URL,
     method: "post",
     params: {
       cid: CID,
